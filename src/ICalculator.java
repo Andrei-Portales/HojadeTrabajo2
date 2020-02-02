4f58c1baeb38cd94c733970f365646d6d41135a2
cesarvin <vinizio007@gmail.com>
2020-02-02 15:55:09 -0600

    Interface para las operaciones de la calculadora

---
+++ src/ICalculator.java
@@ -0,0 +1,10 @@
+public interface ICalculator {
+	
+	public void fillStack(String text, IStack<String> expresion);
+	
+	public void doOperation(); 
+	
+	public void getResult(IStack<String> expresion);
+	
+	
+}
