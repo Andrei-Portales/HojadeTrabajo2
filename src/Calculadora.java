889435c3212d2ca5f5bbcb2326b4cb4e6a9fa784
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-02-01 18:24:19 -0600

    Add files via upload

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -34,7 +34,42 @@ public class Calculadora {
 	}
 	
 	
-	public String suma(String operador,int d1,int d2) {
-		return "";
+	public Integer operar(String operador,String[] datos) {
+		int resultado = 0;
+		
+		switch (operador) {
+		
+		case "suma":
+			for (String dato: datos) {
+				int num = Integer.parseInt(dato);
+				resultado = resultado + num;
+			}
+			break;
+		case "resta":
+			resultado = Integer.parseInt(datos[0]);
+			for (int i = 1;i<=datos.length - 1;i++) {
+				resultado = resultado - Integer.parseInt(datos[i]);
+			}
+			break;
+		case "multiplicacion":
+			resultado++;
+			for (String dato:datos) {
+				resultado = resultado * Integer.parseInt(dato);
+			}
+			break;
+		case "division":
+			resultado = Integer.parseInt(datos[0]);
+			for (int i = 1;i<= datos.length - 1;i++) {
+				resultado = resultado / Integer.parseInt(datos[i]);
+			}
+			break;
+		}
+		return resultado;
 	}
+	
+	
+	
+	
+	
+	
 }
